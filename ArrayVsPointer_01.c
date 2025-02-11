e74e430154430c738e564287a2e205074a9bcde0
Deepak Kumar Yadav <yadav1996deepak1@gmail.com>
2025-02-11 14:40:42 +0000

    ArrayVsPointer

--- ArrayVsPointer_01.c
+++ ArrayVsPointer_01.c
@@ -4,7 +4,7 @@ int add(int array[10])
 {
     printf("size of array =%ld\n",sizeof(array));
     int sum=0;
-    for(int i=0;i<sizeof(array);i++)
+    for(int i=0;i<sizeof(array)/sizeof(int);i++)
     {
         sum=sum+array[i];
     }
@@ -14,7 +14,9 @@ int add(int array[10])
 int main()
 {
     int a[10]= {1,1,1,1,1,1,1,1,1,1};
+    printf("size of array_a =%ld\n",sizeof(a));
     printf("sum: %d\n", add(a));
+
     return 0;
 
 }
