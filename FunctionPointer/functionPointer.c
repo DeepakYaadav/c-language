aeaf13c7d2401752a155b800187722eb978ead97
Deepak Kumar Yadav <yadav1996deepak1@gmail.com>
2025-02-14 04:16:06 +0000

    Function Pointer

---
+++ FunctionPointer/functionPointer.c
@@ -0,0 +1,27 @@
+//function pointer is also a type of callback function. 
+//function pointer point to memory location and content is assumed to be the code or instruction. 
+
+#include<stdio.h>
+void function_1()
+{
+    printf("function_1()\n");   
+}
+
+void function_2()
+{
+    printf("function_2()\n");
+    
+}
+int main()
+{
+    void (*func_ptr)(void); //function pointer declaration. returntype and argument. 
+    //return_dataType(*fun_ptr_Name)(input argument)
+    
+    func_ptr =function_1;
+    func_ptr();
+
+    func_ptr = function_2;
+    func_ptr ();
+
+    return 0;
+}
