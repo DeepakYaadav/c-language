21659c86015d740611273018c0e83d0f27b23de8
Deepak Kumar Yadav <yadav1996deepak1@gmail.com>
2025-02-16 12:38:16 +0000

    Function pointer application

---
+++ FunctionPointer/main.c
@@ -0,0 +1,11 @@
+#include "math_library.h"                       // take library from current directory
+
+
+int main()
+{
+    execute_operation(ADD,100,5);    
+    execute_operation(SUB,100,5);
+    execute_operation(MUL,100,5);
+    execute_operation(DIV,100,5);
+return 0;
+}
---
+++ FunctionPointer/math_library.c
@@ -0,0 +1,32 @@
+#include<stdio.h>                       //take standard libray
+#include "math_library.h"               //take libray from current directory
+
+static void add(int x,int y)
+{
+    printf("add: %d\n",x+y);
+}
+
+static void sub(int x,int y)
+{
+    printf("sub: %d\n",x-y);
+}
+
+static void mul(int x,int y)
+{
+    printf("mul: %d\n",x*y);
+}
+
+static void div(int x,int y)
+{
+    printf("add: %d\n",x/y);
+}
+
+typedef void(*func_t)(int a,int b);           //typecasting of function pointer(func_t)
+
+func_t fun_lib[4] = {add ,sub, mul, div};     // function pointer array fun_lib 
+
+void execute_operation(int operation,int a,int b)
+{
+    fun_lib[operation](a,b);
+}
+
