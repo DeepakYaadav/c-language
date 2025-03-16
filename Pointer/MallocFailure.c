791a9adf442e8de08bc917990de14796f3c4b966
Deepak Kumar Yadav <yadav1996deepak1@gmail.com>
2025-03-16 10:11:41 +0000

    Pointer and DMA

--- Pointer/MallocFailure.c
+++ Pointer/MallocFailure.c
@@ -8,18 +8,17 @@ int main()
 for (int i=0; i<10; i++)
 {
     int *p = (int*)malloc(REQUEST);
-    if (p == NULL)
-    {
-        printf("Memory allocation failed at iteration %d\n", i);
-        return 1;
-    }
-
     // Force the OS to allocate real memory by writing to it
     for (int j=0; j<REQUEST/sizeof(int); j++)
     {
         p[j] = 0;  // Writing forces actual memory allocation
     }
-
+   if (p == NULL)
+    {
+        printf("Memory allocation failed at iteration %d\n", i);
+        return 1;
+    }
     printf("i: %d: %p\n", i, p);
+  
 }
 }
